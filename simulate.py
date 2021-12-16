c7850a9adc9845eac9f7e61866373914b9a51536
mattfusco7 <mattsterf@gmail.com>
2021-12-15 21:38:33 -0500

    changes to simulate

--- simulate.py
+++ simulate.py
@@ -16,23 +16,33 @@ def make_random_move(player_number, game_board):
     action = actions_list[index]
     game_board.make_move(player_number, action)
 
-num_games = 1000
+def make_monte_carlo_move(game_board):
+    game_board2 = copy.deepcopy(game_board)
+    root = MCTSNode(state = game_board2)
+    mcts_move = root.best_action().parent_action
+    game_board.move(mcts_move)
 
+# how many games to run
+num_games = 100
+
+# need try except block because some runs of montecarlo give a random value error
+# other than that, (in the games where montecarlo works) it wins about 96% 
+# and ties 4% of the time
 def simulate():
     mc_win_count = 0
     random_win_count = 0
     error_count = 0
     draw_count = 0
-    for i in range(num_games):
+
+    i = 0
+    while i < num_games + error_count:
+        i += 1
         game_board = create_board()
         try:
             while not game_board.is_game_over():
-                make_random_move(1,game_board)
-                #MCTS move 2
-                game_board2 = copy.deepcopy(game_board)
-                root = MCTSNode(state = game_board2)
-                mcts_move = root.best_action().parent_action
-                game_board.move(mcts_move)
+                # alternate between random move and monte carlo move
+                make_random_move(1, game_board)
+                make_monte_carlo_move(game_board)
             if game_board.won == 2:
                 mc_win_count += 1
             elif game_board.won == -1:
@@ -42,15 +52,9 @@ def simulate():
         except:
             error_count += 1
         
-        
-    # print(visualize_board(game_board))
-    # print("board_status",game_board.board_status)
-    # print("won:", game_board.won)
-
     print("MCTS Win Number: ", mc_win_count)
     print("Random Win Number: ", random_win_count)
     print("Tie Game Number: ", draw_count)
-    print("Number of Errors: ", error_count)
 
 
 if __name__ == "__main__":
